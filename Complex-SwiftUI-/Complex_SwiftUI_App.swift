b6be2a31e6bf5d39c0e90570b0d2b6c5995648ca
den0206 <mac.pop@mineo.jp>
2020-09-03 22:24:34 +0900

    Initial Commit

---
+++ Complex-SwiftUI-/Complex_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Complex_SwiftUI_App.swift
+//  Complex-SwiftUI-
+//
+//  Created by 酒井ゆうき on 2020/09/03.
+//
+
+import SwiftUI
+
+@main
+struct Complex_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Complex-SwiftUI-/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Complex-SwiftUI-
+//
+//  Created by 酒井ゆうき on 2020/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
